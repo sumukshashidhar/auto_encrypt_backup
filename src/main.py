7a6b82230b4f67ac576ed84d9c01211b04901d6c
sumukshashidhar <sumukuuu@gmail.com>
2020-11-09 21:48:35 +0530

    vim-commit

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import argparse
 
 ## init parser
-PARSER_DOCSTRING = """
-Welcome to NCryptBack.
-The one stop shop for all your encrypted backup needs
-"""
+PARSER_DOCSTRING = "\
+Welcome to NCryptBack. \n\
+The one stop shop for all your encrypted backup needs.\n\
+"
 parser = argparse.ArgumentParser(description=PARSER_DOCSTRING)
 parser.parse_args()
