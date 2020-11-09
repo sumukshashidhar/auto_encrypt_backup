768a5cc4452aacb5eb9a664b33e5522eabd396d9
sumukshashidhar <sumukuuu@gmail.com>
2020-11-09 21:48:45 +0530

    added some argparse code, running now

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import argparse
 
 ## init parser
-PARSER_DOCSTRING = "\
-Welcome to NCryptBack. \n\
-The one stop shop for all your encrypted backup needs.\n\
-"
+PARSER_DOCSTRING = """
+Welcome to NCryptBack.
+The one stop shop for all your encrypted backup needs.
+
+"""
+
+
 parser = argparse.ArgumentParser(description=PARSER_DOCSTRING)
 parser.parse_args()
+parser.add_argument("-i", "--inputdir", help="The Input Directory")
+parser.add_argument("-f", "--file", help="An Input File to Encrypt")
+parser.add_argument("-o", "--out", help="The output file")
