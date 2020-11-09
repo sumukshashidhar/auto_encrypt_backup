867b84ffcb154f90d35a3319f62eaf430d0737e2
sumukshashidhar <sumukuuu@gmail.com>
2020-11-09 21:07:52 +0530

    vim-commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+clas Backup:
+    def __init__(self):
+        """
+        Main Init Class for the Program. Very Program based architecture
+        """
+
+
+
+
+if __name__=="__main__":
+    Backup()
